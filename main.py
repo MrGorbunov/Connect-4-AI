99ca82f885565bdfab88a9d1fe3b0408bbfa0844
MrGorbunov <volarcko@gmail.com>
2019-12-05 20:53:08 -0500

    Game logic (Board from boardclass.py) integrated with GUI (main.py)

--- main.py
+++ main.py
@@ -9,20 +9,24 @@ Michael Gorbunov
 
 '''
 TODO
--Create classes for data chunks
--Require almost 0 parameters with most functions
--Adapt to work with Dennis' gamestate
+-Make way to restart / play again
+-Have win showcase / drawing
+   => requires restructuring boardclass to return piece cords
 -Add input from buttons
--Split multiple files
+
 
 -SFX
 '''
 
 from enum import IntEnum
+from boardclass import Board
 import pygame
 
-pygame.init()
 
+
+
+#-------------------------------------- PyGame / Image Globals -----------------------------
+pygame.init()
 
 #window info
 SCALE_FACTOR = 6
@@ -40,26 +44,30 @@ clock = pygame.time.Clock()
 #loading all images
 BG_IMG    = pygame.image.load("img/wall.png")
 BOARD_IMG = pygame.image.load("img/gameboard.png")
-RED_CHIP  = pygame.image.load("img/red_piece.png")
-RED_GHOST = pygame.image.load("img/ghost_red_piece.png")
-YEL_CHIP  = pygame.image.load("img/yel_piece.png")
-YEL_GHOST = pygame.image.load("img/ghost_yel_piece.png")
 
-#these come from the image, representing the x,y of the top left
+#these come from the image, representing the x,y of the top left of the gameboard
 BOARD_X = 22 * SCALE_FACTOR
 BOARD_Y = 21 * SCALE_FACTOR
 CHIP_SIZE = 16 * SCALE_FACTOR
+#distance (px) between the top of the board and the preview piece
+PREVIEW_PADDING = -3 * SCALE_FACTOR
+
+PLAYER_GHOST_IMG = pygame.image.load("img/ghost_red_piece.png")
+PLAYER_PIECE_IMG = pygame.image.load("img/red_piece.png")
+AI_PIECE_IMG     = pygame.image.load("img/yel_piece.png")
+AI_GHOST_IMG     = pygame.image.load("img/ghost_yel_piece.png")
+
 
 #scaling the images up
 BG_IMG    = pygame.transform.scale(BG_IMG, (SCREEN_WIDTH, SCREEN_HEIGHT))
 BOARD_IMG = pygame.transform.scale(BOARD_IMG, (SCREEN_WIDTH, SCREEN_HEIGHT))
-#image size of 16x16 assumed
-RED_CHIP  = pygame.transform.scale(RED_CHIP,  (CHIP_SIZE, CHIP_SIZE)) 
-RED_GHOST = pygame.transform.scale(RED_GHOST, (CHIP_SIZE, CHIP_SIZE))
-YEL_CHIP  = pygame.transform.scale(YEL_CHIP,  (CHIP_SIZE, CHIP_SIZE))
-YEL_GHOST = pygame.transform.scale(YEL_GHOST, (CHIP_SIZE, CHIP_SIZE))
+#image size of 16x16 assumed for the pieces
+PLAYER_PIECE_IMG = pygame.transform.scale(PLAYER_PIECE_IMG,  (CHIP_SIZE, CHIP_SIZE)) 
+PLAYER_GHOST_IMG = pygame.transform.scale(PLAYER_GHOST_IMG, (CHIP_SIZE, CHIP_SIZE))
+AI_PIECE_IMG     = pygame.transform.scale(AI_PIECE_IMG,  (CHIP_SIZE, CHIP_SIZE))
+AI_GHOST_IMG     = pygame.transform.scale(AI_GHOST_IMG, (CHIP_SIZE, CHIP_SIZE))
 
-#draw BG
+#draw BG => then later it just gets overwritten in specific places
 screen.blit(BG_IMG, (0,0))
 screen.blit(BOARD_IMG, (0,0))
 
@@ -69,67 +77,53 @@ screen.blit(BOARD_IMG, (0,0))
 
 
 
-#-------------------------------------- Game Setup -----------------------------
+
+#-------------------------------------- Animation & Game Globals -----------------------------
 #used to act based on current situation
-class GameState(IntEnum):
-	PLAYER_MOVE = 0
-	AI_MOVE = 1
-	ANIMATION = 2
+GAME_STATE = {
+	"PLAYER_MOVE": 0,
+	"AI_MOVE": 1,
+	"ANIMATION": 2
+}
 
-#this way we're always dealing with ints but its readable as text
-class BoardPiece(IntEnum):
-	EMPTY = 0
-	RED = 1
-	YELLOW = 2
+#stores what input type comes from what keys
+INPUT_TYPE = {
+	"LEFT": pygame.K_LEFT,
+	"RIGHT": pygame.K_RIGHT,
+	"SELECT": pygame.K_DOWN
+}
 
-#stores what input comes from what keys
-class InputType:
-	LEFT   = pygame.K_LEFT
-	RIGHT  = pygame.K_RIGHT
-	SELECT = pygame.K_DOWN
-	HOME   = pygame.K_SPACE
-
-game_state = GameState.PLAYER_MOVE
-
+game_state = GAME_STATE['PLAYER_MOVE']
 preview_move = 0 #which row the current move is planned to be in
 
-player_turn  = True
-
-PLAYER_PIECE = BoardPiece.RED
-PLAYER_CHIP  = RED_CHIP
-PLAYER_GHOST = RED_GHOST
-
-AI_PIECE = BoardPiece.YELLOW
-AI_CHIP = YEL_CHIP
-AI_GHOST = YEL_GHOST
-
-
-#distance (in px) between top of board and preview piece. positive is up
-PREVIEW_PADDING = -3 * SCALE_FACTOR
-
-FPS = 60
 
 #these track the piece that has just been dropped so as to animate it
-anim_piece_pos = (0, 0)
-anim_piece_y_vel = 0
-anim_piece_img = RED_CHIP
-anim_piece_destination = (0, 0)
-bounces = 0
 GRAVITY = 20 * SCALE_FACTOR
-ACCEL_Y = GRAVITY * (1.0 / FPS)
+FPS = 60
+
+#amount of times it should bounce
+fall_anim = {
+	"pos": (0, 0),
+	"dest": (0, 0),
+	"y_vel": 0,
+	"Y_ACCEL": GRAVITY * (1.0 / FPS),
+	"bounce": 0,
+	"img": PLAYER_PIECE_IMG,
+}
+NUM_BOUNCES = 2
+BOUNCE_COEF = -0.3
 
 
 #setup the board
-board = () #board[0][0] = top left, [6][5] = bot right
-for col in range(8):
-	cur_row = ()
-	for row in range(7):
-		cur_row += (BoardPiece.EMPTY,)
-	board += (cur_row,)
+board = Board()
 
+#Ideally these wouldn't be here
+cur_piece_img = PLAYER_PIECE_IMG
+cur_ghost_img = PLAYER_GHOST_IMG
 
-
-
+#start with player
+if board.isCompTurn():
+	board.changeTurn()
 
 
 
@@ -140,7 +134,6 @@ def draw_piece(pos, chip):
 	'''Draws a piece on the board. pos is a (px_x, px_y) Visually overwrites any piece that was in the location. Assumes chip is an image of CHIP_SIZE x CHIP_SIZE dimensiom'''
 	global board, screen
 	
-	pos
 	# the area that the chip takes up on the board, (x, y, width, height)
 	board_area = pos + (CHIP_SIZE, CHIP_SIZE)
 
@@ -158,8 +151,6 @@ def clear_piece(pos):
 	
 	screen.blit(BG_IMG, pos, board_area)
 	screen.blit(BOARD_IMG, pos, board_area)
-
-
 
 
 #utility draw functions
@@ -181,78 +172,113 @@ def get_board_cords(px_x, px_y):
 
 
 #----------------------------- Coupled Drawing Commands ----------------------------------
-def handle_move_preview_input(chip, ghost, goingLeft):
-	'''Will adjust preview_move and update visuals to match the new state. pos is a (px_x, px_y)'''
-	global preview_move, PREVIEW_PADDING
+def handle_move_preview_input(new_preview_move):
+	'''Will update visuals to match the new state. pos is a (px_x, px_y). new_preview_move is automatically clamped (%= 7)'''
+	global PREVIEW_PADDING, preview_move
 
+	#clamping for 0-6
+	new_preview_move %= 7
+	
 	#clear the current ghost and preview piece
-	clear_piece(get_px_cords(preview_move, 5))
+	ghost_height = board.empty_slots_in_col(preview_move) - 1
+	clear_piece(get_px_cords(preview_move, ghost_height))
 	clear_piece(get_px_cords(preview_move, -1, PREVIEW_PADDING))
 	
-	if goingLeft:
-		preview_move -= 1
-	else:
-		preview_move += 1
-	#keeps in range 0-6
-	preview_move %= 7
+	#adjusting move
+	preview_move = new_preview_move
 	
 	#preview piece, is above the board
-	draw_piece(get_px_cords(preview_move, -1, PREVIEW_PADDING), chip)
-	draw_piece(get_px_cords(preview_move, 5), ghost)
+	draw_piece(get_px_cords(preview_move, -1, PREVIEW_PADDING), cur_piece_img)
+	#this check needs to happen again because it's a new column
+	ghost_height = board.empty_slots_in_col(preview_move) - 1	
+	draw_piece(get_px_cords(preview_move, ghost_height), cur_ghost_img)
 
 
 def handle_falling_animation():
 	'''Will draw and update values relating to a falling piece'''
-	global anim_piece_pos, anim_piece_y_vel, anim_piece_img, anim_piece_destination
-	global ACCEL_Y, bounces
+	global fall_anim
 	
 	#clear where the piece might've been last frame
-	clear_piece(anim_piece_pos)
+	clear_piece(fall_anim['pos'])
 	
 	#compute physics
-	anim_piece_pos = list(anim_piece_pos)
+	fall_anim['pos'] = list(fall_anim['pos'])
 	
-	anim_piece_y_vel += ACCEL_Y
-	anim_piece_pos[1] += anim_piece_y_vel
+	fall_anim['y_vel'] += fall_anim['Y_ACCEL']
+	fall_anim['pos'][1] += fall_anim['y_vel']
 	
 	#check if it has reached the target y yet
-	if anim_piece_destination[1] <= anim_piece_pos[1]:
-		anim_piece_pos[1] = anim_piece_destination[1]
+	if fall_anim['dest'][1] <= fall_anim['pos'][1]:
+		#this way it isn't below the line
+		fall_anim['pos'][1] = fall_anim['dest'][1]
 	
-		#this makes it bounce twice
-		if bounces > 1:
-			global game_state
-			game_state = GameState.PLAYER_MOVE
-			bounces = 0
+		#bouncing behaviour
+		if fall_anim['bounce'] + 1 >= NUM_BOUNCES:
+			finish_fall_animation()
 		else:
-			bounces += 1
+			fall_anim['bounce'] += 1
 			#reverse dir and lose some speed
-			anim_piece_y_vel *= -0.4
+			fall_anim['y_vel'] *= BOUNCE_COEF
 		
 	#redraw
-	anim_piece_pos = tuple(anim_piece_pos)
-	draw_piece(anim_piece_pos, anim_piece_img)
-	
+	fall_anim['pos'] = tuple(fall_anim['pos'])
+	draw_piece(fall_anim['pos'], fall_anim['img'])
 
+
+def finish_fall_animation():
+	'''Wrap the falling animation, changing game_state and redrawing what's necessary'''
+	global game_state
+	
+	game_state = GAME_STATE['PLAYER_MOVE']
+	fall_anim['bounce'] = 0
+	handle_move_preview_input(preview_move)
+
+	#win check
+	if board.isWinner():
+		global running
+		running = False
+	
+		for i in range(5):
+			print("We have a winner!!!")
 
 def set_falling_animation_parameters():
 	'''Sets all parameters relating to a falling animation. Assumes this is being called when a piece is at the top (just after preview piece)'''
-	global anim_piece_pos, anim_piece_y_vel, anim_piece_img, anim_piece_destination
-	global game_state
+	global fall_anim, game_state
+	global AI_PIECE_IMG, PLAYER_PIECE_IMG
 	
-	game_state = GameState.ANIMATION
+	game_state = GAME_STATE['ANIMATION']
 	
-	if player_turn:
-		anim_piece_img = PLAYER_CHIP
+	#set current piece
+	if board.isCompTurn():
+		fall_anim['img'] = AI_PIECE_IMG
 	else:
-		anim_piece_img = AI_CHIP
+		fall_anim['img'] = PLAYER_PIECE_IMG
 	
-	anim_piece_pos = get_px_cords(preview_move, -1, PREVIEW_PADDING)
-	anim_piece_y_vel = 0
+	fall_anim['pos'] = get_px_cords(preview_move, -1, PREVIEW_PADDING)
+	fall_anim['y_vel'] = 0
 	
 	#set to just be the bottom of the board
-	anim_piece_destination = get_px_cords(preview_move, 5)
+	fall_anim['dest'] = get_px_cords(preview_move, board.empty_slots_in_col(preview_move) - 1)
 	
+
+
+
+
+
+
+#--------------------------------- Game Logic Commands --------------------------------	
+def handle_move_logic():
+	'''Updates the Board instance (board) and cur_images'''
+	global cur_piece_img, cur_ghost_img
+	board.editBoard(preview_move)
+	board.changeTurn()
+	
+	if board.isCompTurn():
+		cur_piece_img = AI_PIECE_IMG
+		cur_ghost_img = AI_GHOST_IMG
+	else:
+		cur_piece_img = PLAYER_PIECE_IMG
+		cur_ghost_img = PLAYER_GHOST_IMG
 	
 
 
@@ -281,32 +307,34 @@ while running:
 		
 		
 		#doesn't listen to input, but still allows for closing the window
-		if game_state == GameState.ANIMATION:
+		if game_state == GAME_STATE['ANIMATION']:
 			break
 		
 		
 		#something pressed
 		if event.type == pygame.KEYDOWN:
-			if event.key == InputType.LEFT:
-				handle_move_preview_input(PLAYER_CHIP, PLAYER_GHOST, goingLeft=True)
+			if event.key == INPUT_TYPE['LEFT']:
+				#clamping the new move is handled by the function
+				handle_move_preview_input(preview_move - 1)
 				
-			elif event.key == InputType.RIGHT:
-				handle_move_preview_input(PLAYER_CHIP, PLAYER_GHOST, goingLeft=False)
+			elif event.key == INPUT_TYPE['RIGHT']:
+				handle_move_preview_input(preview_move + 1)
 
-			elif event.key == InputType.SELECT:
+			elif event.key == INPUT_TYPE['SELECT']:
 				#choosing to drop a piece
 				set_falling_animation_parameters()
+				handle_move_logic()
+				#so that anything else doesn't muck things up
 				break
-				
-			elif event.key == InputType.HOME:
-				print("home")
 	
 	
 	#update based on game_state
-	if game_state == GameState.ANIMATION:
+	if game_state == GAME_STATE['ANIMATION']:
 		handle_falling_animation()
 	
 	pygame.display.update()
 	clock.tick(FPS)
 
 
+#this way there's a delay and you can admire the victory
+asd = input()
